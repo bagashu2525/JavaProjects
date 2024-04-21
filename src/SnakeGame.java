601def71b341f42149bd4aa502fac2a75ae5dad5
bagashu2525 <bagasmita25@gmal.com>
2024-04-22 03:41:47 +0530

    JPanel

---
+++ src/SnakeGame.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.random.*;
+
+import javax.swing.JPanel;
+
+public class SnakeGame extends JPanel{
+     
+}
