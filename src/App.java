9508e35b2739cd524e67d557cd5bdb0f5f856674
bagashu2525 <bagasmita25@gmal.com>
2024-04-22 01:33:44 +0530

    Window Creation

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import javax.swing.*;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int width = 600;
+        int height = 600;
+
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setSize(width,height);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
